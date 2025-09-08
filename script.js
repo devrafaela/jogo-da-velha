414f248370c029ed9fee315c7c545ab50fa34e93
Rafaela <rafaelagaames@Gmail.com>
2025-09-08 14:27:10 -0300

    feat: acrescentando funcionalidade de vencer e declarar empate

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
 const cellElements = document.querySelectorAll("[data-cell]");
 const board = document.querySelector("[data-board]");
+const winningMessageTextElement = document.querySelector("[data-winning-message-text]");
+const winningMessage = document.querySelector("[data-winning-message]");
 
 let isCircleTurn;
+
+const winnerCombinations = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
 
 const startGame = () => {
   for (const cell of cellElements) {
@@ -10,6 +23,26 @@ const startGame = () => {
   isCircleTurn = false;
 
   board.classList.add("x");
+}
+
+const endGame = (isDraw) => {
+  if (isDraw) {
+    winningMessageTextElement.innerText = "Empate!";
+  } else {
+    winningMessageTextElement.innerText = isCircleTurn 
+      ? " 'O' venceu! " 
+      : " 'X' venceu! ";
+  }
+
+  winningMessage.classList.add("show-winning-message");
+}
+
+const checkForWin = (currentPlayer) => {
+  return winnerCombinations.some(combination => {
+    return combination.every(index => {
+      return cellElements[index].classList.contains(currentPlayer);
+    })
+  })
 }
 
 const placeMark = (cell, classToAdd) => {
@@ -37,6 +70,10 @@ const handleClick = (e) => {
   placeMark(cell, classToAdd);
   
   // Verificar vitória
+  const isWin = checkForWin(classToAdd);
+  if (isWin) {
+    endGame(false);
+  }
   // Verificar empate  
 
   // Mudar simbolo
